6027329d42e25abae104639394142cee0de8b4cc
Ismail Baoud <ibaoud46@gmail.com>
2024-10-07 20:22:18 +0100

    version 1

---
+++ main-projet-de-sas-2.js
@@ -0,0 +1,9 @@
+document.getElementById('button').addEventListener('click', function() {
+    const nom = getElementById('nom').value;
+    const prenom = getElementById('pnom').value;;
+    const telephone = getElementById('tele').value;;
+    let age = getElementById('age').value;;
+    let statut = getElementById('statut').value;;
+    let referance_uniqeu = getElementById('r').value;;
+    let date_de_reservation = getElementById('date').value;
+});
